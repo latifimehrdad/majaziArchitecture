b4c2a9773de05040225500801961fcaf1ae82fff
Mehrdad <m3.latifi@gmail.com>
2022-01-11 12:01:21 +0330

    تغییر زیرساخت 1400/10/21

--- latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/utility/Splitter.kt
+++ latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/utility/Splitter.kt
@@ -6,20 +6,28 @@ import java.text.DecimalFormat
 class Splitter {
 
     //______________________________________________________________________________________________ split
-    fun split(value : String) : String{
-        if (value.isNullOrEmpty())
+    fun split(value : String): String{
+        if (value.isEmpty())
             return ""
-        var s = value.replace(",","",false).replace("٬","",false)
+        val s = value.replace(",","",false).replace("٬","",false)
         return if (s.isDigitsOnly())
-            split(s.toInt())
+            split(s.toLong())
         else
             ""
     }
     //______________________________________________________________________________________________ split
 
     //______________________________________________________________________________________________ split
-    fun split(value : Int) :String {
+    fun split(value : Long): String {
         val dec = DecimalFormat("#,###")
+        return dec.format(value)
+    }
+    //______________________________________________________________________________________________ split
+
+
+    //______________________________________________________________________________________________ split
+    fun split(value: Double): String {
+        val dec = DecimalFormat("#,###.########")
         return dec.format(value)
     }
     //______________________________________________________________________________________________ split
