3260e692ad8054b89f1c2a24b4c2fdba6f80b47e
Mehrdad <m3.latifi@gmail.com>
2022-01-12 11:54:52 +0330

    تغییر زیرساخت 1400/10/22

--- app/src/main/java/land/majazi/majaziarchitecture/MainActivity.java
+++ app/src/main/java/land/majazi/majaziarchitecture/MainActivity.java
@@ -1,16 +1,9 @@
 package land.majazi.majaziarchitecture;
 
 import android.os.Bundle;
-import android.util.Log;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import land.majazi.latifiarchitecure.views.activity.MasterActivity;
-import land.majazi.latifiarchitecure.views.customs.text.MLTextView;
-import land.majazi.latifiarchitecure.views.fragments.FR_Primary;
-
-
-public class MainActivity extends MasterActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
