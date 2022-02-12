32c697a3b535a35fc38e5f89a4ead6219986abe3
LC300 <m3.latifi@gmail.com>
2022-02-12 11:50:30 +0330

    اضافه کردن dns

---
+++ latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/enums/IPvMode.kt
@@ -0,0 +1,15 @@
+package land.majazi.latifiarchitecure.enums
+
+enum class IPvMode(val code: String) {
+    SYSTEM("system"),
+    IPV6_FIRST("ipv6"),
+    IPV4_FIRST("ipv4"),
+    IPV6_ONLY("ipv6only"),
+    IPV4_ONLY("ipv4only");
+
+    companion object {
+        @JvmStatic
+        fun fromString(ipMode: String): IPvMode =
+            values().find { it.code == ipMode } ?: throw Throwable("Unknown value $ipMode")
+    }
+}
