634ae10ecd96d5dcb7d8724f3e44deb716ac2176
LC300 <m3.latifi@gmail.com>
2022-06-28 11:26:24 +0430

    تغییر جدا کننده اعداد

--- latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/utility/Splitter.kt
+++ latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/utility/Splitter.kt
@@ -1,24 +1,30 @@
 package land.majazi.latifiarchitecure.utility
 
 import androidx.core.text.isDigitsOnly
+import java.lang.Exception
 import java.text.DecimalFormat
 
 class Splitter {
 
     //______________________________________________________________________________________________ split
-    fun split(value : String): String{
+    fun split(value: String): String {
         if (value.isEmpty())
             return ""
-        val s = value.replace(",","",false).replace("٬","",false)
+        val s = value.replace(",", "", false).replace("٬", "", false)
         return if (s.isDigitsOnly())
             split(s.toLong())
         else
-            ""
+            try {
+                val temp = s.toDouble()
+                split(temp)
+            } catch (e: Exception) {
+                ""
+            }
     }
     //______________________________________________________________________________________________ split
 
     //______________________________________________________________________________________________ split
-    fun split(value : Long): String {
+    fun split(value: Long): String {
         val dec = DecimalFormat("#,###")
         return dec.format(value)
     }
