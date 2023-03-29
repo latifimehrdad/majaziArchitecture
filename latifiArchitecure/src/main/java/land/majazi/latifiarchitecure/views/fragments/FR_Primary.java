448dc87d4622ad66af00c3ddfc27e9d7f296b56f
m-latifi <m3.latifi@gmail.com>
2023-03-29 14:08:59 +0430

    خطای احتمالی لودینگ

--- latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/views/fragments/FR_Primary.java
+++ latifiArchitecure/src/main/java/land/majazi/latifiarchitecure/views/fragments/FR_Primary.java
@@ -215,18 +215,23 @@ public class FR_Primary extends Fragment implements FragmentAction{
     //______________________________________________________________________________________________ setRecyclerLoading
     public void setRecyclerLoading(RecyclerView recyclerLoading, int layout, @ColorRes int Color, int count) {
 
-        AP_Loading ap_loading = new AP_Loading();
-        recyclerLoading.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
-        skeletonScreen = Skeleton.bind(recyclerLoading)
-                .adapter(ap_loading)
-                .load(layout)
-                .shimmer(true)      // whether show shimmer animation.                      default is true
-                .count(count)          // the recycler view item count.                        default is 10
-                .color(Color)       // the shimmer color.                                   default is #a2878787
-                .angle(0)          // the shimmer angle.                                   default is 20;
-                .duration(2000)     // the shimmer animation duration.                      default is 1000;
-                .frozen(false)
-                .show();
+        try{
+            AP_Loading ap_loading = new AP_Loading();
+            recyclerLoading.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
+            skeletonScreen = Skeleton.bind(recyclerLoading)
+                    .adapter(ap_loading)
+                    .load(layout)
+                    .shimmer(true)      // whether show shimmer animation.                      default is true
+                    .count(count)          // the recycler view item count.                        default is 10
+                    .color(Color)       // the shimmer color.                                   default is #a2878787
+                    .angle(0)          // the shimmer angle.                                   default is 20;
+                    .duration(2000)     // the shimmer animation duration.                      default is 1000;
+                    .frozen(false)
+                    .show();
+        } catch (Exception ignored){
+
+        }
+
     }
     //______________________________________________________________________________________________ setRecyclerLoading
 
@@ -245,13 +250,18 @@ public class FR_Primary extends Fragment implements FragmentAction{
     //______________________________________________________________________________________________ setViewLoading
     public void setViewLoading(View view, int layout, @ColorRes int Color) {
 
-        viewSkeletonScreen = Skeleton.bind(view)
-                .load(layout)
-                .angle(0)
-                .color(Color)
-                .duration(2200)
-                .shimmer(true)
-                .show();
+        try {
+            viewSkeletonScreen = Skeleton.bind(view)
+                    .load(layout)
+                    .angle(0)
+                    .color(Color)
+                    .duration(2200)
+                    .shimmer(true)
+                    .show();
+        } catch (Exception ignored){
+
+        }
+
     }
     //______________________________________________________________________________________________ setViewLoading
 
